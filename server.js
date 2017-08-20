b8f95e07707ba87f26ba247d6626d628b0027389
hackersassasin <31130655+hackersassasin@users.noreply.github.com>
2017-08-20 19:57:53 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -25,11 +25,16 @@ var articleOne = {
                         This is the content of my First Article
                     </p>    `
 };
-
-var htmlTemplate=1{`
+function createTemplate (data)
+{
+var title=data.title;
+var date=data.date;
+var heading=data.heading;
+var content=data.content;
+var htmlTemplate=1`
         <html>
             <head>
-                <title>Article-one</title>
+                <title>${title}</title>
                 <meta name="viewport" content="width=device-width, initial-scale=1"/>
                 <link href="/ui/style.css" rel="stylesheet" />
         
@@ -39,43 +44,21 @@ var htmlTemplate=1{`
                 <div class="container">
                         <div><a href="/">Home</a></div>
                             <hr/>
-                            <h3>Article One</h3>
-                        <div>Aug-20-2017</div> 
-                        <div>
-                            <p> This is the content of my First Article
-                                This is the content of my First Article 
-                                This is the content of my First Article 
-                                This is the content of my First Article 
-                                This is the content of my First Article 
-                                This is the content of my First Article
-                            </p>
-                            <p> This is the content of my First Article
-                                This is the content of my First Article 
-                                This is the content of my First Article 
-                                This is the content of my First Article 
-                                This is the content of my First Article 
-                                This is the content of my First Article
-                            </p>
-                            </div>
+                            <h3>${heading}</h3>
+                            <div>${date}</div> 
+                            <div>${content}</div>
                 </div>
             
         </html>  `
-    
+    return htmlTemplate;
     
 }
-
-
-
-
-
-
-
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article-one' ,function (req, res) {
-    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+    res.send(createTemplate(articleOne));
 });
 
 app.get('/article-two' ,function (req, res) {
