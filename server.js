e3bc289a62f9ac6de63f631851929ab7dbb87c97
hackersassasin <31130655+hackersassasin@users.noreply.github.com>
2017-08-20 20:40:26 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -104,8 +104,9 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/:articleName' ,function (req, res) {
-    res.send(createTemplate(articles[article]));
     var articleName = req.params.articleName;
+    res.send(createTemplate(articles[articleName]));
+    
     
 });
 
